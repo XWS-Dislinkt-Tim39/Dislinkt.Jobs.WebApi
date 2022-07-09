9c288d3bb3642b34b548636b1ae77e528da5da66
Pedja <despotovicpedja@gmail.com>
2022-07-09 07:40:23 +0200

    removed-description-from-entity

--- Dislinkt.Jobs.Persistence.Neo4j/Entites/JobEntity.cs
+++ Dislinkt.Jobs.Persistence.Neo4j/Entites/JobEntity.cs
@@ -6,7 +6,6 @@ namespace Dislinkt.Jobs.Persistence.Neo4j.Entities
     public class JobEntity : BaseEntity
     {
         public string PositionName { get; set; }
-        public string Description { get; set; }
         public Seniority Seniority { get; set; }
         public Job ToJob()
             => new Job(this.Id, this.PositionName, this.Seniority);
