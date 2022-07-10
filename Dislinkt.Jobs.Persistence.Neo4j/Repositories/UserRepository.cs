42043bd903bb6702ae23bfd3b803b3c7ab2016b3
Pedja <despotovicpedja@gmail.com>
2022-07-10 16:23:36 +0200

    createUser

--- Dislinkt.Jobs.Persistence.Neo4j/Repositories/UserRepository.cs
+++ Dislinkt.Jobs.Persistence.Neo4j/Repositories/UserRepository.cs
@@ -1,4 +1,7 @@
 ﻿using Dislinkt.Jobs.Core.Repositories;
+using Dislinkt.Jobs.Domain.Users;
+using Dislinkt.Jobs.Persistence.Neo4j.Common;
+using Dislinkt.Jobs.Persistence.Neo4j.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,16 @@ namespace Dislinkt.Jobs.Persistence.Neo4j.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly IQueryExecutor _queryExecutor;
+
+        public UserRepository(IQueryExecutor queryExecutor)
+        {
+            _queryExecutor = queryExecutor;
+        }
+
         public async Task AddUserAsync(User user)
         {
-
+            await _queryExecutor.CreateAsync<UserEntity>(UserEntity.ToUserEntity(user), "USER");
         }
     }
 }
