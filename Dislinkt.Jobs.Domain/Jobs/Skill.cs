c578f114707ae11c26ee763333a7d696ab0712d9
Pedja <despotovicpedja@gmail.com>
2022-07-10 16:00:54 +0200

    skill-entity

---
+++ Dislinkt.Jobs.Domain/Jobs/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Jobs.Domain.Jobs
+{
+    public class Skill
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+        public Skill(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Dislinkt.Jobs.Persistence.Neo4j/Entities/SkillEntity.cs
@@ -0,0 +1,24 @@
+﻿using Dislinkt.Jobs.Domain.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Jobs.Persistence.Neo4j.Entities
+{
+    public class SkillEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public Skill ToSkill()
+            => new Skill(this.Id, this.Name);
+        public static SkillEntity ToSkillEntity(Skill skill)
+        {
+            return new SkillEntity
+            {
+                Id = skill.Id,
+                Name = skill.Name
+            };
+        }
+    }
+}
