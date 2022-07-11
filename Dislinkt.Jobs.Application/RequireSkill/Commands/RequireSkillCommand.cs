b42ab5a8d80ba44efd3817145c7ed17db94ad20f
CyberWho <despot@uns.ac.rs>
2022-07-11 12:29:50 +0200

    require-skill

---
+++ Dislinkt.Jobs.Application/RequireSkill/Commands/RequireSkillCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Dislinkt.Jobs.Application.RequireSkill.Commands
+{
+    public class RequireSkillCommand : IRequest<bool>
+    {
+
+        public RequireSkillCommand(RequireSkillData requireSkillData)
+        {
+            Request = requireSkillData;
+        }
+
+        public RequireSkillData Request;
+    }
+}
---
+++ Dislinkt.Jobs/RequireSkillData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislinkt.Jobs
+{
+    public class RequireSkillData
+    {
+        /// <summary>
+        /// ID of the skill that's being required.
+        /// </summary>
+        public Guid SkillId { get; set; }
+        /// <summary>
+        /// ID of the job the skill is being required from.
+        /// </summary>
+        public Guid JobId { get; set; }
+    }
+}
