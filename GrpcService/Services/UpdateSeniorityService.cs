15448a77e4bf433444c931b1e4bcd74774baa42f
predragdespotovic <despotovicpedja@gmail.com>
2022-08-29 20:33:13 +0200

    add service

---
+++ GrpcService/Services/UpdateSeniorityService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Dislinkt.Jobs.Core.Repositories;
+using Dislinkt.Jobs.Domain.Jobs;
+using Grpc.Core;
+using GrpcUpdateSeniorityService;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class UpdateSeniorityService : UpdateSeniorityGreeter.UpdateSeniorityGreeterBase
+    {
+        private readonly ILogger<UpdateSeniorityService> _logger;
+        private readonly IUserRepository _userRepository;
+        public UpdateSeniorityService(ILogger<UpdateSeniorityService> logger, IUserRepository userRepository) 
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+        }
+
+        public override async Task<UpdateSeniorityReply> UpdateSeniority(UpdateSeniorityRequest request, ServerCallContext context)
+        {
+            try
+            {
+                await _userRepository.UpdateSeniorityById(Guid.Parse(request.Id), (Seniority)request.Seniority);
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return await Task.FromResult(new UpdateSeniorityReply
+            {
+                Message = $"User: {request.Id} | new seniority: {(Seniority)request.Seniority}",
+                Successful = true
+            });
+        }
+    }
+}
+
